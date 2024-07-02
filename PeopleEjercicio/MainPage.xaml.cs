fd2d899193d0838e5812db594ee8bd27c1a7bcb4
MatyxxdxUDLA <matias.robayo@udla.edu.ec>
2024-07-01 21:17:18 -0500

    Add project files.

---
+++ PeopleEjercicio/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using PeopleEjercicio.Models;
+using System.Collections.Generic;
+
+namespace PeopleEjercicio
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public async void OnNewButtonClicked(object sender, EventArgs args)
+        {
+            MRStatusMessage.Text = "";
+
+            await App.PersonRepo.AddNewPerson(MRNewPerson.Text);
+            MRStatusMessage.Text = App.PersonRepo.StatusMessage;
+        }
+
+        public async void OnGetButtonClicked(object sender, EventArgs args)
+        {
+            MRStatusMessage.Text = "";
+
+            List<Person> people = await App.PersonRepo.GetAllPeople();
+            MRPeopleList.ItemsSource = people;
+        }
+    }
+
+}
